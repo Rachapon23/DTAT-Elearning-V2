5951ba6ca5447266c8ae9af80fbe12a7f1e4bb3d
DIAT-WSC <ohm32070@gmail.com>
2023-06-08 09:22:56 +0700

    fix nav admin now can go to home admin, fix upload file

--- client/src/component/layout/NavTeacher.js
+++ client/src/component/layout/NavTeacher.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -6,11 +6,73 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import './Nav.css'
 import { useNavigate } from "react-router-dom";
 import { Button, Dropdown } from 'antd';
+import { routeAdmin } from "../../function/funcroute";
+import { routeTeacher } from "../../function/funcroute";
+import { routeUser } from "../../function/funcroute";
 
 const NavTeacher = () => {
 
   const navigate = useNavigate();
 
+  const CheckAdmin = async () => {
+    let isAdmin = false
+    await routeAdmin(sessionStorage.getItem("token"))
+      .then((res) => {
+        if (res.data.status) {
+          isAdmin = res.data.status
+          navigate(`/admin/home`)
+        }
+        else {
+          isAdmin = false
+        }
+      })
+      .catch(() => {
+        isAdmin = false
+      })
+    return isAdmin
+  }
+
+  const CheckTeacher = async () => {
+    let isTeacher = false
+    await routeTeacher(sessionStorage.getItem("token"))
+      .then((res) => {
+        if (res.data.status) {
+          isTeacher = true
+          navigate(`/teacher/home`)
+        }
+        else {
+          isTeacher = false
+        }
+      })
+      .catch(() => {
+        isTeacher = false
+      })
+    return isTeacher
+  }
+
+  const CheckUser = async () => {
+    let isUser = false
+    await routeUser(sessionStorage.getItem("token"))
+      .then((res) => {
+        if (res.data.status) {
+          isUser = res.data.status
+          navigate(`/student/home`)
+        }
+        else {
+          isUser = false
+        }
+      })
+      .catch(() => {
+        isUser = false
+      })
+    return isUser
+  }
+
+  const handleClickLogo = async (e) => {
+    if (await CheckAdmin()) return
+    if (await CheckTeacher()) return
+    if (await CheckUser()) return
+  }
 
   const items = [
     {
@@ -33,8 +95,8 @@ const NavTeacher = () => {
   return (
     <Navbar className="bg-nav" expand="lg">
       <Container>
-        <Navbar.Brand href="/teacher/home" className="text-white">
-          <img src="/navbrand3.png" className="logo-nav" />
+        <Navbar.Brand className="text-white" onClick={handleClickLogo} style={{ cursor: "pointer" }}>
+          <img src="/navbrand3.png" className="logo-nav" alt="logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
